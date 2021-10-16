38698384f7691518b5e77e824006d99de748b83f
os <admin@tencent.com>
2021-10-16 21:53:17 +0800

    update

--- p.sh
+++ p.sh
@@ -1,12 +1,14 @@
 export USE_SSH=true
 
+export GIT_USER=richardgong1987
+
+export DEPLOYMENT_BRANCH=gh-pages
+
 git add .
 git commit -m "update"
 git push
 
 git config --global --unset http.proxy
-
-DEPLOYMENT_BRANCH=gh-pages
 
 yarn deploy
 
